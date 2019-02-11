6e4a411c8e4c9ae5851ee2756a09c2b4e7cee163
rahisulhaque <rahisul@ilcoud.com>
2019-02-11 01:51:33 -0700

    start

---
+++ ex1.c
@@ -0,0 +1,24 @@
+/* ***************************************************************************************** */
+/*                                                                                           */
+/*                                                                                           */
+/*   ex1.c                                                           __    _            __   */
+/*                                                       _________ _/ /_  (_)______  __/ /   */
+/*   By: rahisulhaque <rahisul@icloud.com>              / ___/ __ `/ __ \/ / ___/ / / / /    */
+/*                                                     / /  / /_/ / / / / (__  ) /_/ / /     */
+/*   Created: 2019/02/10 11:38:34 by rahisulhaque     /_/   \__,_/_/ /_/_/____/___,_/_/      */
+/*   Updated: 2019/02/10 11:46:05 by rahisulhaque                                            */
+/*                                                                                           */
+/* ***************************************************************************************** */
+
+#include <stdio.h>
+
+int 	main(int argc, char *argv[]){
+	int distance = 200;
+	
+	printf("Hello there!\n");
+	printf("This message shows that C program is working.\n");
+	printf("You are  %d miles away from me.\n", distance);
+	
+	return(0);
+}
+
